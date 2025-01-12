78288a2415a9d1a5d7e985d7eca6649976d10a6f
unknown <medisaagum@.gmail.com>
2025-01-12 15:52:51 +0700

    penambahan bagian hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,7 @@
 //import React from 'react'
+import Button from "./ui/Button";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
@@ -15,11 +17,38 @@ const Hero = () => {
       </div>
 
       <div className="h-screen w-full dark:bg-black-100 bg-white 
-      dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+      dark:bg-grid-white/[0.2] bg-grid-black/[0.2] flex items-center justify-center
+      absolute top-0 left-0">
         <div className="absolute pointer-events-none inset-0 flex items-center 
         justify-center dark:bg-black bg-white 
-        [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
+        [mask-image:radial-gradient(ellipse_at_center,
+        transparent_20%,black)]"/>
       </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center">
+          <h2 className="upperacase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Halo saya Agum
+          </h2>
+
+          <TextGenerateEffect
+            className="text-center text-[400]
+          md:text-5xl lg:text-6xl"
+            words="Ini adalah aku" />
+
+          <p className="text-center md:tracking-wider mb-4 
+          text-sm md:text-lg lg:text-2xl">
+          Hi, I'am Agum, a Devops Engineer with some experience
+          </p>
+          <a href="#about">
+            <Button/>
+
+          </a>
+        </div>
+
+
+      </div>
+
     </div>
   );
 };
---
+++ components/ui/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = () => {
+    return (
+        <button className="px-8 py-2  bg-black text-white text-sm rounded-md font-semibold hover:bg-black/[0.8] hover:shadow-lg">
+            Show my Work
+        </button>
+
+    )
+}
+
+export default Button
